d6e4fdec7e3ef3a8c2f0349cca49d3497873ac32
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-08-10 09:05:17 -0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.dominio.BootCamp;
 import org.example.dominio.Curso;
+import org.example.dominio.Dev;
 import org.example.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,10 +25,41 @@ public class Main {
         mentoria.setDescrição("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
 
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setName("Bootcamp Java Developer");
+        bootCamp.setDescrição("Descrição Bootcamp Java Developer");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
 
+        Dev devEricsson = new Dev();
+        devEricsson.setName("Ericsson");
+        devEricsson.inscreverBootCamp(bootCamp);
+        System.out.println("Conteudos inscritos" + devEricsson.getConteudosInscritos());
+        devEricsson.progress();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos" + devEricsson.getConteudosInscritos());
+        System.out.println("Conteudos concluidos" + devEricsson.getConteudosConcluidos());
+        System.out.println("XP:" + devEricsson.calcularTotalXp());
+
+        System.out.println("-----------------------------------");
+
+        Dev devTainara = new Dev();
+        devTainara.setName("Tainara");
+        devTainara.inscreverBootCamp(bootCamp);
+        System.out.println("Conteudos inscritos" + devTainara.getConteudosInscritos());
+        devTainara.progress();
+        devTainara.progress();
+        devTainara.progress();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos" + devTainara.getConteudosInscritos());
+        System.out.println("Conteudos concluidos" + devTainara.getConteudosConcluidos());
+        System.out.println("XP:" + devTainara.calcularTotalXp());
     }
 }
